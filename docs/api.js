1ac2ce895ea96b80ac79d73643441f25287bbed0
bma <me@bjoernacker.de>
2014-04-23 01:41:18 +0200

    initial commit

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Room",
+        "Server",
+        "Session"
+    ],
+    "modules": [
+        "gordon"
+    ],
+    "allModules": [
+        {
+            "displayName": "gordon",
+            "name": "gordon"
+        }
+    ]
+} };
+});
